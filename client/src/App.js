07ecc81163a640a372116024fee5e6c45f09be50
Jon Zachary <jontzachary@gmail.com>
2019-07-17 18:12:36 -0700

    remove CRA boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -16,22 +15,10 @@ function App() {
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+   
         <p>
          {JSON.stringify(weather)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
   );
 }
 
